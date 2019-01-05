3e5afc8ec7d42770b8c913fe1c3fa71b5646650a
AnastasiaVorton <anastasia.vorton@gmail.com>
2019-01-05 21:29:17 +0200

    Fix styles of top navigation. Add more style selectors. Add animated underline in menus. Add top navigation color change on scroll.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,42 @@
-window.onscroll = function() {myFunction()};
+// Top navigation is done referencing https://stackoverflow.com/questions/6848269/using-jquery-to-find-current-visible-element-and-update-navigation
+
+window.onscroll = function () {
+    navigationHandler()
+};
 
 // Get the header
-var header = document.getElementById("nav-list");
+let header = document.getElementById("nav-list");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+let sticky = header.offsetTop;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > window.innerHeight) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+function navigationHandler() {
+    if (window.pageYOffset > window.innerHeight) {
+        header.classList.add("sticky");
+
+        let cutoff = $(window).scrollTop();
+        let curSec = $.find('.current');
+        let curID = $(curSec).attr('id');
+
+        if (curID === "projects" || curID === "bio") {
+            $('.top-nav-a').addClass('white');
+        } else {
+            $('.top-nav-a').removeClass('white')
+        }
+
+
+        $('.section').each(function () {
+            if ($(this).offset().top + $(this).height() > cutoff) {
+                $('.section').removeClass('current');
+                $(this).addClass('current');
+                return false; // stops the iteration after the first one on screen
+            }
+        });
+
+        document.getElementById("nav-list").style.backgroundColor = getComputedStyle(document.getElementById(curID)).backgroundColor;
+
+    } else {
+        header.classList.remove("sticky");
+    }
 }
 
-
-// $(window).bind('scroll', function () {
-//     if ($(window).scrollTop() > $( window ).height()) {
-//         $('.menu').addClass('sticky');
-//     } else {
-//         $('.menu').removeClass('sticky');
-//     }
-// });
